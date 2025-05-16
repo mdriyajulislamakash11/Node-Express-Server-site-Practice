7c9f40cecc4db186ac1ef68ca7399b2af3bd13cb
Akash <hafejmohammadakash@gmail.com>
2025-05-16 18:38:54 +0600

    users name ekta variable baniye setake ami web page a dekhte parsi

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const port = 5000
 
 app.use(cors())
 
-
+const users = [
+  {id: 1, name: "akash", email: " @gmail.com"},
+  {id: 2, name: "samiul", email: "smaiul@gmail.com"},
+  {id: 3, name: "alif", email: "alif@gmail.com"},
+]
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+
+app.get("/users", (rec, res) => {
+  res.send(users)
+});
+
+
 
 
 
